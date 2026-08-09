10b90575f484cd47694b8b5b594fbce2fa199ecf
Anthony Tucker <54104732+ATphobia22@users.noreply.github.com>
2026-08-09 11:07:48 -0500

    chore: initialize evidence package

---
+++ src/evidence/__init__.py
@@ -0,0 +1,10 @@
+from .evidence_graph import EvidenceGraph, EvidenceEdge, ProvenanceRecord
+from .archimedes_authority import ArchimedesAuthority, ArchimedesCalculation
+
+__all__ = [
+    "EvidenceGraph",
+    "EvidenceEdge",
+    "ProvenanceRecord",
+    "ArchimedesAuthority",
+    "ArchimedesCalculation",
+]
